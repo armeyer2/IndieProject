f4a7d8054686f48241759a250dd34cb1ee7bee79
Aaron Meyer <armeyer2@madisoncollege.edu>
2018-02-25 13:04:44 -0600

    First commit after rolling back

--- src/main/Java/entity/User.java
+++ src/main/Java/entity/User.java
@@ -1,27 +1,26 @@
 package entity;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.ejb.Local;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
- * TODO At minimum, complete the following
- * TODO create no-arg constructor
- * TODO create getters and setters
- * TODO add hibernate annotations
- * TODO Javadoc comments
+ * A class to represent a user.
+ *
+ * @author armeyer2
  */
-
-@Entity(name = "userEntity")
+@Entity(name = "User")
 @Table(name = "user")
-
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    private int id;
 
     @Column(name = "first_name")
     private String firstName;
@@ -29,59 +28,200 @@ public class User {
     @Column(name = "last_name")
     private String lastName;
 
-    @Column(name = "birth_year")
-    private int birthYear;
+    @Column(name = "user_name", unique = true, nullable = false)
+    private String userName;
 
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
-    @GenericGenerator(name = "native", strategy = "native")
-    private int id;
+    /**
+     * Bidirectional @OneToMany
 
+     The bidirectional @OneToMany association also requires a @ManyToOne association on the child side.
+     Although the Domain Model exposes two sides to navigate this association, behind the scenes,
+     the relational database has only one foreign key for this relationship.
+
+     Every bidirectional association must have one owning side only (the child side),
+     the other one being referred to as the inverse (or the mappedBy) side.
+
+     Foreign key is on the child table (Order in this example)
+
+     Source: http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#associations-one-to-many
+     */
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<Order> orders = new HashSet<>();
+
+    /**
+     * Instantiates a new User.
+     */
     public User() {
-        final Logger logger = LogManager.getLogger(this.getClass());
-
 
     }
 
-    public User(String firstName, String lastName, int birthYear) {
+    /**
+     * Instantiates a new User.
+     *
+     * @param firstName   the first name
+     * @param lastName    the last name
+     * @param userName    the user name
+     * @param dateOfBirth the date of birth
+     */
+    public User(String firstName, String lastName, String userName, LocalDate dateOfBirth) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.birthYear = birthYear;
-
+        this.userName = userName;
+        this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * Gets first name.
+     *
+     * @return the first name
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets first name.
+     *
+     * @param firstName the first name
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets last name.
+     *
+     * @return the last name
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets last name.
+     *
+     * @param lastName the last name
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
-    public int getBirthYear() {
-        return birthYear;
+    /**
+     * Gets user name.
+     *
+     * @return the user name
+     */
+    public String getUserName() {
+        return userName;
     }
 
-    public void setBirthYear(int birth_year) {
-        this.birthYear = birth_year;
+    /**
+     * Sets user name.
+     *
+     * @param userName the user name
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public int getId() {
         return id;
     }
 
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
     public void setId(int id) {
         this.id = id;
     }
 
-}
+    /**
+     * Gets date of birth.
+     *
+     * @return the date of birth
+     */
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
 
+    /**
+     * Sets date of birth.
+     *
+     * @param dateOfBirth the date of birth
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    /**
+     * Gets age.
+     *
+     * @return the age
+     */
+    public int getAge() {
+
+        return (int)ChronoUnit.YEARS.between(dateOfBirth, LocalDate.now());
+    }
+
+    /**
+     * Gets orders.
+     *
+     * @return the orders
+     */
+    public Set<Order> getOrders() {
+        return orders;
+    }
+
+    /**
+     * Sets orders.
+     *
+     * @param orders the orders
+     */
+    public void setOrders(Set<Order> orders) {
+        this.orders = orders;
+    }
+
+    /**
+     * Add a order.
+     *
+     * @param order the order to add
+     */
+    public void addOrder(Order order) {
+        orders.add( order );
+        order.setUser( this );
+    }
+
+    /**
+     * Remove a order.
+     *
+     * @param order the order to remove
+     */
+    public void removeOrder(Order order) {
+        orders.remove( order );
+        order.setUser( null );
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", userName='" + userName + '\'' +
+                ", id=" + id +
+                ", dateOfBirth=" + dateOfBirth +
+                ", age=" + getAge() +
+                // ", orders=" + getorders() +
+                '}';
+    }
+}
